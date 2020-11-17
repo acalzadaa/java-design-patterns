d0da580dbf61d687ab04ebbe290b2b0b97425d25
Alejandro Calzada <alejandro.calzada.algravez@gmail.com>
2020-11-17 16:03:53 -0600

    add observer design pattern.

---
+++ java-design-patterns/src/main/java/com/core/creational/observer/demo/Observer.java
@@ -0,0 +1,9 @@
+package com.core.creational.observer.demo;
+
+public class Observer {
+
+	public void update() {
+		System.out.println("flag value changed in Subject");
+	}
+
+}
---
+++ java-design-patterns/src/main/java/com/core/creational/observer/demo/ObserverPatternEx.java
@@ -0,0 +1,22 @@
+package com.core.creational.observer.demo;
+
+public class ObserverPatternEx {
+	public static void main(String[] args) {
+		System.out.println("*** Observer Pattern demo ***");
+		Observer o1 = new Observer();
+		Subject sub1 = new Subject();
+		sub1.register(o1);
+
+		System.out.println("Set flag to 5");
+		sub1.setFlag(5);
+
+		System.out.println("Set flag to 25");
+		sub1.setFlag(25);
+
+		sub1.unregister(o1);
+
+		System.out.println("Set flag to 10");
+		sub1.setFlag(10);
+
+	}
+}
