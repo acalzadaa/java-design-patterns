c675a51ffd29af0cf445b4f408f7e86b547dd7d9
Alejandro Calzada <alejandro.calzada.algravez@gmail.com>
2020-11-18 18:42:29 -0600

    add decorator design pattern.

---
+++ src/main/java/com/core/adapter/CalculatorAdapter.java
@@ -0,0 +1,15 @@
+package com.core.adapter;
+
+public class CalculatorAdapter {
+	Calculator calculator;
+	Triangle triangle;
+
+	public double getArea(Triangle t) {
+		calculator = new Calculator();
+		triangle = t;
+		Rect r = new Rect();
+		r.l = triangle.b;
+		r.w = 0.5 * triangle.h;
+		return calculator.getArea(r);
+	}
+}
---
+++ src/main/java/com/core/adapter/Rect.java
@@ -0,0 +1,6 @@
+package com.core.adapter;
+
+class Rect {
+	public double l;
+	public double w;
+}
